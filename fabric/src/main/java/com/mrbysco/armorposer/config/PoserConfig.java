07054a3c0902105c535260260e5d298fe4cd2669
Mrbysco <Mrbysco@users.noreply.github.com>
2024-07-28 21:11:14 +0200

    Fix tooltips #20

--- fabric/src/main/java/com/mrbysco/armorposer/config/PoserConfig.java
+++ fabric/src/main/java/com/mrbysco/armorposer/config/PoserConfig.java
@@ -3,6 +3,7 @@ package com.mrbysco.armorposer.config;
 import com.mrbysco.armorposer.Reference;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.CollapsibleObject;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
@@ -14,14 +15,19 @@ public class PoserConfig implements ConfigData {
 	public General general = new General();
 
 	public static class General {
+		@ConfigEntry.Gui.Tooltip
 		@Comment("Show the Armor Stand configuration GUI on shift right click")
 		public boolean enableConfigGui = true;
+		@ConfigEntry.Gui.Tooltip
 		@Comment("Allow Armor Stand to be renamed using name tags")
 		public boolean enableNameTags = true;
+		@ConfigEntry.Gui.Tooltip
 		@Comment("Allow scrolling to add / decrease an angle value in the posing screen")
 		public boolean allowScrolling = true;
+		@ConfigEntry.Gui.Tooltip
 		@Comment("Restrict the ability to resize the Armor Stand to server operators")
 		public boolean restrictResizeToOP = false;
+		@ConfigEntry.Gui.Tooltip
 		@Comment("List of players that are allowed to resize the Armor Stand when restrictResizeToOP is enabled")
 		public List<String> resizeWhitelist = List.of();
 	}
