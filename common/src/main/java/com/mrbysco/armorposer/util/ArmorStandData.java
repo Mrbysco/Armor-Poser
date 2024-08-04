55f5e7a9a68abf2e409fa2e192bad46e25809fbd
Mrbysco <Mrbysco@users.noreply.github.com>
2024-08-04 20:02:58 +0200

    Update ArmorStandData.java

--- common/src/main/java/com/mrbysco/armorposer/util/ArmorStandData.java
+++ common/src/main/java/com/mrbysco/armorposer/util/ArmorStandData.java
@@ -45,7 +45,7 @@ public class ArmorStandData {
 			this.rotation = compound.getList("Rotation", CompoundTag.TAG_FLOAT).getFloat(0);
 		}
 		if (compound.contains("Pose")) {
-			CompoundTag poseTag = (CompoundTag) compound.get("Pose");
+			CompoundTag poseTag = compound.getCompound("Pose");
 
 			String[] keys = new String[]{"Head", "Body", "LeftLeg", "RightLeg", "LeftArm", "RightArm"};
 			for (int i = 0; i < keys.length; i++) {
